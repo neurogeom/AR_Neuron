8d6ece5366476a522363e6a2d3b73f537575f1a3
OhridBlack <37727776+OhridBlack@users.noreply.github.com>
2020-12-23 11:06:45 +0800

    Create TestSocket.cs

---
+++ Android/Scripts/TestSocket.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Net;
+using Command;
+using UnityEngine.UI;
+using System.Threading;
+/*
+Created by BlackFJ
+*/
+
+///<summary>
+///
+///</summary>
+public class TestSocket : MonoBehaviour
+{
+    public ClientSocket mSocket = new ClientSocket();
+    public static List<string> swcList = new List<string>();
+
+    private void Start()
+    {
+        //Debug.Log("start1");
+        //mSocket.ConnectServer("192.168.1.2", 8088);
+        //Debug.Log("start2");
+        //mSocket.SendMessage("ConnectionMobilePhone:192.168.1.2:1234");
+        //Debug.Log("start3");
+        //Debug.Log("SendMessageOver!");
+        //mSocket.RequestLoop();
+        GetComponent<Button>().onClick.AddListener(ButtonClick);
+    }
+
+    private void ButtonClick()
+    {
+        mSocket.ConnectServer("192.168.1.2", 8088);
+        mSocket.SendMessage("ConnectionMobilePhone:192.168.1.2:1234");
+        ThreadStart ts = new ThreadStart(ThreadWorkLoop);
+        Thread tr = new Thread(ts);
+        tr.Start();
+    }
+
+    public void ThreadWorkLoop()
+    {
+        mSocket.RequestLoop();
+        /*while (mSocket.IsConnected)
+        {
+            //string data = "";
+            //SendMessage(data);
+
+            try
+            {
+                string ans = mSocket.ReceiveResponse();
+                //Debug.Log(ans);
+                CommandAnalysis ca = new CommandAnalysis(ans);
+                //ca.Show();
+                switch (ca.commandType)
+                {
+                    case CommandType.FileListShow:
+                        //Debug.Log("fileListShow");
+
+                        for (int i = 0; i < ca.fileList.Length; ++i)
+                        {
+
+                            swcList.Add(ca.fileList[i]);
+                        }
+                        AddSelectionButton(swcList);
+                        break;
+                    default:
+                        Debug.Log("Nothing happened!");
+                        break;
+                }
+            }
+            catch
+            {
+                mSocket.IsConnected = false;
+            }
+        }*/
+    }
+
+    public void AddSelectionButton(List<string> swcFile)
+    {
+        if (swcFile.Count == 0) return;
+        GameObject canvas = GameObject.Find("Canvas");
+        for(int i = 0; i < swcFile.Count; ++i)
+        {
+            GameObject button = new GameObject("Button" + i.ToString(),
+                typeof(Button), typeof(RectTransform), typeof(Image));
+            button.transform.SetParent(canvas.transform);
+        }
+    }
+
+    /*private void Update()
+    {
+        if (mSocket.IsConnected)
+        {
+            try
+            {
+                string command=mSocket.ReceiveResponse();
+                Debug.Log("Receive data: " + command);
+                if (command != null)
+                {
+                    
+                    CommandAnalysis ca = new CommandAnalysis(command);
+                    
+                    switch (ca.commandType)
+                    {
+                        case CommandType.FileListShow:
+                            //Debug.Log("FileListShow!");
+                            string returnCommand = "FileSelection:" + ca.fileList[0];
+                            mSocket.SendMessage(returnCommand);
+                            break;
+                        case CommandType.SWCTransmition:
+
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            }
+            catch
+            {
+                mSocket.IsConnected = false;
+            }
+        }
+    }*/
+}
