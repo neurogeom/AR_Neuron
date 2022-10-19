b1e3a41a0d1c44fd7a2aacf24e26ccf0d9736486
jockeyc <80121168@qq.com>
2022-10-19 20:36:24 +0800

    10.19 20:36

--- Assets/Scripts/DebugWindow.cs
+++ Assets/Scripts/DebugWindow.cs
@@ -1,52 +1,52 @@
-﻿using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-namespace MRTK.Tutorials.AzureSpatialAnchors
-{
-    public class DebugWindow : MonoBehaviour
-    {
-        [SerializeField] private TextMeshProUGUI debugText = default;
+//namespace MRTK.Tutorials.AzureSpatialAnchors
+//{
+//    public class DebugWindow : MonoBehaviour
+//    {
+//        [SerializeField] private TextMeshProUGUI debugText = default;
 
-        private ScrollRect scrollRect;
-        string text;
-        bool needUpdate = false;
+//        private ScrollRect scrollRect;
+//        string text;
+//        bool needUpdate = false;
 
-        private void Start()
-        {
-            // Cache references
-            scrollRect = GetComponentInChildren<ScrollRect>();
+//        private void Start()
+//        {
+//            // Cache references
+//            scrollRect = GetComponentInChildren<ScrollRect>();
 
-            // Subscribe to log message events
+//            // Subscribe to log message events
 
-            //Application.logMessageReceived += HandleLog;
-            Application.logMessageReceivedThreaded += HandleLog;
+//            //Application.logMessageReceived += HandleLog;
+//            Application.logMessageReceivedThreaded += HandleLog;
 
-            // Set the starting text
-            debugText.text = "Debug messages will appear here.\n\n";
-        }
+//            // Set the starting text
+//            debugText.text = "Debug messages will appear here.\n\n";
+//        }
 
-        private void Update()
-        {
-            if (needUpdate)
-            {
-                debugText.text += text;
-                Canvas.ForceUpdateCanvases();
-                scrollRect.verticalNormalizedPosition = 0;
-                needUpdate = false;
-                text = "";
-            }
-        }
+//        private void Update()
+//        {
+//            if (needUpdate)
+//            {
+//                debugText.text += text;
+//                Canvas.ForceUpdateCanvases();
+//                scrollRect.verticalNormalizedPosition = 0;
+//                needUpdate = false;
+//                text = "";
+//            }
+//        }
 
-        private void OnDestroy()
-        {
-            Application.logMessageReceived -= HandleLog;
-        }
+//        private void OnDestroy()
+//        {
+//            Application.logMessageReceived -= HandleLog;
+//        }
 
-        private void HandleLog(string message, string stackTrace, LogType type)
-        {
-            text += message + " \n";
-            needUpdate = true;
-        }
-    }
-}
+//        private void HandleLog(string message, string stackTrace, LogType type)
+//        {
+//            text += message + " \n";
+//            needUpdate = true;
+//        }
+//    }
+//}
